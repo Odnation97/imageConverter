a4980e0d314bed0f80f069df9a562ad96a180362
= <=>
2018-12-25 13:40:54 +0000

    Added MVC format

---
+++ src/Controller/mainInterfaceController.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author o_noo
+ */
+
+public class mainInterfaceController implements Initializable {
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
